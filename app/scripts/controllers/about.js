1e15d6212dc438c16ad407f064eac73678386dbe
Xunzhi Sun <xunzhi.sun@gmail.com>
2015-10-07 22:05:11 -0700

    first commit after yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name budgetAdventureApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the budgetAdventureApp
+ */
+angular.module('budgetAdventureApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name budgetAdventureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the budgetAdventureApp
+ */
+angular.module('budgetAdventureApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
